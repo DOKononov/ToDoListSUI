8787a48288fcec9d0afe16c61cacdb1b82373914
d.o.kononov <d.o.kononov@gmail.com>
2025-04-11 17:49:24 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Dmitry Kononov on 11.04.25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
